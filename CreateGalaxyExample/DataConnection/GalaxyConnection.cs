f26a711e9eaa4c7369b7d6a0741848eb59bc314c
isak.skeie <245362@usn.no>
2022-08-09 16:50:19 +0200

    Moved connect and login, Creatd key for galaxy in config

--- CreateGalaxyExample/DataConnection/GalaxyConnection.cs
+++ CreateGalaxyExample/DataConnection/GalaxyConnection.cs
@@ -8,5 +8,36 @@ namespace CreateGalaxyExample.DataConnection
 {
     public class GalaxyConnection
     {
+        public void Connect()
+        {
+
+            
+            IGalaxies gals = grAccess.QueryGalaxies(nodeName);
+            Console.WriteLine("Accessing Galaxy...");
+            if (gals == null || grAccess.CommandResult.Successful == false)
+            {
+                Console.WriteLine(grAccess.CommandResult.CustomMessage + grAccess.CommandResult.Text);
+                return;
+            }
+
+            IGalaxy galaxy = gals[galaxyName];
+
+
+            ICommandResult cmd;
+        }
+        
+        public void Login()
+        {
+            galaxy.Login("", "");
+            Console.WriteLine("Logging into Galaxy...");
+            cmd = galaxy.CommandResult;
+            if (!cmd.Successful)
+            {
+                Console.WriteLine("Login to galaxy Example1 Failed :" +
+                                  cmd.Text + " : " +
+                                  cmd.CustomMessage);
+                return;
+            }
+        }
     }
 }
